d3474eb8e2d5af7486e2ed834ac6a77f9044a8b6
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2021-06-25 17:22:58 +0200

    Fix script on 64-bit systems

--- geonames_elasticsearch_loader.py
+++ geonames_elasticsearch_loader.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from tqdm import tqdm
 import time
 
-csv.field_size_limit(sys.maxsize)
+csv.field_size_limit(2**31 - 1)
 es = Elasticsearch(urls='http://localhost:9200/', timeout=60, max_retries=2)
 
 
